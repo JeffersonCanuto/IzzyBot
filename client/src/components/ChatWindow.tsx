862377a26ff37a0cfda769bc52381e6c7acd5212
jefferson <jeffersondscanuto@gmail.com>
2025-08-19 14:26:31 -0300

    feat: implement ChatWindow component as container for messages

--- client/src/components/ChatWindow.tsx
+++ client/src/components/ChatWindow.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import type { Message as MessageType } from "@/types";
+import Message from "./Message";
 
-const ChatWindow:React.FC = () => {
+interface ChatWindowProps {
+    messages: MessageType[];
+}
+
+const ChatWindow:React.FC<ChatWindowProps> = ({ messages }) => {
     return (
-        <div>ChatWindow</div>
+        <div className="flex flex-col gap-2 p-4 h-[500px] overflow-y-auto border rounded-md bg-white">
+            {messages.map(msg => (
+                <Message key={msg.id} message={msg} />
+            ))}
+        </div>
     );
 }
 
