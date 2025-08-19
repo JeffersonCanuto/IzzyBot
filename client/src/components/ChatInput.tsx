84024398249857c0274d6b188bdbb9ad5c91f578
jefferson <jeffersondscanuto@gmail.com>
2025-08-19 16:39:26 -0300

    feat: implement ChatInput component for input box of sending messages

--- client/src/components/ChatInput.tsx
+++ client/src/components/ChatInput.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import { forwardRef } from "react";
 
-const ChatInput:React.FC = () => {
-    return (
-        <div>ChatInput</div>
-    );
+interface ChatInputProps {
+    handleSend: () => void;
 }
+
+const ChatInput = forwardRef<HTMLInputElement, ChatInputProps>(({ handleSend }, ref) => {
+    return (
+        <div className="flex gap-2 mt-2">
+            <input
+                ref={ref}
+                type="text"
+                className="flex-1 border rounded-md p-2"
+                placeholder="Type your message..."
+                onKeyDown={e => {
+                    if (e.key === "Enter") {
+                        e.preventDefault();
+
+                        handleSend();
+                    }
+                }}
+            />
+            <button
+                className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                onClick={handleSend}
+            >
+                Send
+            </button>
+        </div>
+    );
+})
 
 export default ChatInput;
