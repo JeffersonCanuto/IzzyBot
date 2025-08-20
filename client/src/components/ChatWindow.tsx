12235b85c8e9c044746d8a2f0347f2b30fadd9f5
jefferson <jeffersondscanuto@gmail.com>
2025-08-20 16:07:07 -0300

    feat: add chat background image

--- client/src/components/ChatWindow.tsx
+++ client/src/components/ChatWindow.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import type { Message as MessageType } from "@/types";
 import Message from "./Message";
 
+import BackgroundImage from "@/assets/chat-background.jpg";
+
 interface ChatWindowProps {
     messages: MessageType[];
 }
 
 const ChatWindow:React.FC<ChatWindowProps> = ({ messages }) => {
     return (
-        <div className="flex flex-col gap-2 p-4 h-[500px] overflow-y-auto border rounded-md bg-white">
+        <div
+            className="flex flex-col gap-2 p-4 h-[500px] overflow-y-auto border rounded-md bg-white"
+            style={{
+                backgroundImage: `url(${BackgroundImage})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center"
+            }}
+        >
             {messages.map(msg => (
                 <Message key={msg.id} message={msg} />
             ))}
