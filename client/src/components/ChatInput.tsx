ab5c226cdcc572dd15fb3693c90c84b48489b593
jefferson <jeffersondscanuto@gmail.com>
2025-08-20 16:09:13 -0300

    style: update styles on ChatInput component

--- client/src/components/ChatInput.tsx
+++ client/src/components/ChatInput.tsx
@@ -1,4 +1,5 @@
 import { forwardRef } from "react";
+import { IoSend } from "react-icons/io5";
 
 interface ChatInputProps {
     handleSend: () => void;
@@ -10,7 +11,7 @@ const ChatInput = forwardRef<HTMLInputElement, ChatInputProps>(({ handleSend }, 
             <input
                 ref={ref}
                 type="text"
-                className="flex-1 border rounded-md p-2"
+                className="flex-1 font-sans border rounded-md p-2 outline-none focus:border-gray-400"
                 placeholder="Type your message..."
                 onKeyDown={e => {
                     if (e.key === "Enter") {
@@ -19,12 +20,13 @@ const ChatInput = forwardRef<HTMLInputElement, ChatInputProps>(({ handleSend }, 
                         handleSend();
                     }
                 }}
+                autoFocus
             />
             <button
-                className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                className="bg-green-500 text-white px-3 py-2 rounded-full hover:bg-green-600 hover:scale-105 transition-all duration-200"
                 onClick={handleSend}
             >
-                Send
+                <IoSend />
             </button>
         </div>
     );
