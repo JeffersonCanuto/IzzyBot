1a3e71e16c166287503c642aca61f4d0421d4c3a
jefferson <jeffersondscanuto@gmail.com>
2025-08-20 16:53:05 -0300

    styles: fix styles on ChatWindow component

--- client/src/components/ChatWindow.tsx
+++ client/src/components/ChatWindow.tsx
@@ -12,17 +12,17 @@ interface ChatWindowProps {
 const ChatWindow:React.FC<ChatWindowProps> = ({ messages }) => {
     return (
         <div className="flex flex-col">
-            <div className="flex flex-row gap-5 items-center bg-gray-50 border border-gray-300 rounded-md py-4 px-5">
+            <div className="flex flex-row gap-3 items-center bg-gray-50 border border-gray-300 rounded-md py-3 px-5">
                 <div className="w-14 h-14 rounded-full border-2 overflow-hidden flex items-center justify-center">
                     <img src={IzzyBot} alt="IzzyBot" className="w-10 h-10 object-cover" />
                 </div>
                 <div>
                     <p className="text-gray-500 font-bold">IzzyBot</p>
-                    <p className="text-[11px] text-gray-600">Online | 23:59</p>
+                    <p className="text-[11px] text-green-600">Online</p>
                 </div>
             </div>
             <div
-                className="flex flex-col gap-2 p-4 h-[410px] overflow-y-auto border rounded-md bg-white"
+                className="flex flex-col gap-2 p-4 h-[418px] overflow-y-auto border rounded-md bg-white"
                 style={{
                     backgroundImage: `url(${BackgroundImage})`,
                     backgroundSize: "cover",
