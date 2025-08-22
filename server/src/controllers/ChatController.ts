d620e8a99098e31d394b1612e7342253fd146c91
jefferson <jeffersondscanuto@gmail.com>
2025-08-22 15:59:13 -0300

    feat: implement ChatController to create payload

--- server/src/controllers/ChatController.ts
+++ server/src/controllers/ChatController.ts
@@ -1,13 +1,32 @@
-import HttpStatusCodes from '@src/constants/HttpStatusCodes';
-import ChatService from '@src/services/ChatService';
+import RouterAgent from "@src/services/RouterAgent";
+import HttpStatusCodes from "@src/constants/HttpStatusCodes";
 
-import { IReq, IRes } from '@src/types';
+import { IReq, IRes } from "@src/types";
+import { AgentPayload } from "@src/types/agents";
 
-async function create(req: IReq, res: IRes) {
-  const { data: { message } } = req.body;
-  await ChatService.create(message);
-  
-  res.status(HttpStatusCodes.CREATED).end();
+/**
+ * Receives client payload and forwards it to RouterAgent
+ */
+class ChatController {
+	static async createPayload(req:IReq, res:IRes) {
+		try {
+			const payload = req.body as unknown as AgentPayload;
+
+			// Validate received client payload
+			if (!payload.message || !payload.user_id || !payload.conversation_id) {
+				return res.status(HttpStatusCodes.BAD_REQUEST).json({ error: "Invalid request payload" });
+			}
+
+			// Forward received payload to RouterAgent
+			const response = await RouterAgent.handleMessage(payload);
+
+			// Return response from RouterAgent
+			return res.status(HttpStatusCodes.OK).json(response);
+		} catch(error:any) {
+			console.error("ChatController error: ", error);
+			return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Internal Server Error" });
+		}
+	}
 }
 
-export default { create } as const;
+export default ChatController;
