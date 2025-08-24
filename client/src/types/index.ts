462e969f488a23a454c048d4b77d750d764ccc8b
jefferson <jeffersondscanuto@gmail.com>
2025-08-24 20:45:55 -0300

    refactor: update typing structure

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,13 +1,13 @@
+export interface Conversation {
+    id: string;
+    messages: Message[];
+    title?: string;
+}
+
 export interface Message {
     id: string;
     text: string;
     sender: "user" | "agent";
     agent?: "KnowledgeAgent" | "MathAgent";
     createdAt?: string;
-}
-
-export interface Conversation {
-    id: string;
-    messages: Message[];
-    title?: string;
 }
