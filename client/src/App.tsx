afc25ea68269948eda50c3c33c47f8370ca07550
jefferson <jeffersondscanuto@gmail.com>
2025-08-24 20:47:23 -0300

    feat: wrap the app with QueryClientProvider from TanStack Query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ChatPage from '@/pages/ChatPage';
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 const App:React.FC = () => {
-    return <ChatPage />
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ChatPage />
+        </QueryClientProvider>
+    );
 }
 
 export default App;
