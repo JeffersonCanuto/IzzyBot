b0616f5caf695f1537b5e22a9c2a324238466ddc
jefferson <jeffersondscanuto@gmail.com>
2025-08-25 20:40:07 -0300

    feat: implement controller to get conversations from Redis

--- server/src/controllers/ChatController.ts
+++ server/src/controllers/ChatController.ts
@@ -4,6 +4,8 @@ import HttpStatusCodes from "@src/constants/HttpStatusCodes";
 import { IReq, IRes } from "@src/types";
 import { UserPayload } from "@src/types/agents";
 
+import { getAllUserConversations } from "@src/services/conversation";
+
 /**
  * Receives client payload and forwards it to RouterAgent
  */
@@ -25,6 +27,21 @@ class ChatController {
 		} catch(error:any) {
 			console.error("ChatController error: ", error);
 			return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Internal Server Error" });
+		}
+	}
+	static async getConversations(req:IReq, res:IRes) {
+		try {
+			const { user_id } = req.query;
+			
+			if (!user_id) {
+				return res.status(HttpStatusCodes.BAD_REQUEST).json({ "error": "user_id is required" });
+			}
+
+			const conversations = await getAllUserConversations(user_id.toString());
+			return res.status(HttpStatusCodes.OK).json({ conversations });
+		} catch(error:any) {
+			console.error("Error fetching user conversations: ", error);
+			return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({ "error": "Failed to fetch user conversations" });
 		}
 	}
 }
