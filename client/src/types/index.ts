a31db6528356a2a32d7a39916f776247049300da
jefferson <jeffersondscanuto@gmail.com>
2025-08-25 09:55:39 -0300

    feat: update shared typing structure

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -11,3 +11,15 @@ export interface Message {
     agent?: "KnowledgeAgent" | "MathAgent";
     createdAt?: string;
 }
+
+export interface Answer {
+    response: string;
+    source_agent_response: string;
+    agent_workflow: {agent: string; decision:string;}[]
+}
+
+export interface SendMessagePayload {
+    message: string;
+    user_id: string;
+    conversation_id: string | null;
+}
