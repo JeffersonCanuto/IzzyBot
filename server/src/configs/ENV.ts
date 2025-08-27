5516a365fba18c1bbf59a9a91b8f38f5aadb7584
jefferson <jeffersondscanuto@gmail.com>
2025-08-27 08:04:44 -0300

    chore: latest changes on env vars

--- server/src/configs/ENV.ts
+++ server/src/configs/ENV.ts
@@ -1,10 +1,6 @@
 import jetEnv, { num, str } from 'jet-env';
-import { isEnumVal } from 'jet-validators';
-
-import { NodeEnvs } from '.';
 
 const ENV = jetEnv({
-  NodeEnv: isEnumVal(NodeEnvs),
   ClientOuterPort: num,
   ServerInnerPort: num,
   OpenAiApiKey: str,
--- server/src/configs/index.ts
+++
@@ -1,6 +0,0 @@
-// These need to match the names in ".env" files
-export enum NodeEnvs {
-  Dev = 'dev',
-  Test = 'test',
-  Production = 'prod'
-}
