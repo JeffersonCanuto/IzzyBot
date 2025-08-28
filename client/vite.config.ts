1c1632b63191188d6a86f52a838bf54cf0c2a475
jefferson <jeffersondscanuto@gmail.com>
2025-08-28 19:23:50 -0300

    chore: enable HTTPS with self-signed SSL certificates

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,13 +3,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import fs from "fs";
 
-// https://vite.dev/config/
+const key = fs.readFileSync(path.resolve(__dirname, "certs/key.pem"));
+const cert = fs.readFileSync(path.resolve(__dirname, "certs/cert.pem"));
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     }
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    https: { key, cert }
   }
 });
