9c87b7961030eb57aef51f691194e56189cdc880
jefferson <jeffersondscanuto@gmail.com>
2025-08-29 06:55:12 -0300

    chore: enable HTTPs using self-signed SSL certificates on the backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,23 @@
+import fs from "fs";
+import https from "https";
 import logger from 'jet-logger';
 
 import ENV from '@src/configs/ENV';
 import server from './server';
 
 const SERVER_START_MSG = `Express server started on port: ${ENV.ServerInnerPort}`;
+const options = {
+  key: fs.readFileSync("certs/key.pem"),
+  cert: fs.readFileSync("certs/cert.pem")
+};
 
 // Start the server
-server.listen(ENV.ServerInnerPort, err => {
-  if (err) {
-    logger.err(err.message);
-  } else {
-    logger.info(SERVER_START_MSG);
-  }
+const httpsServer = https.createServer(options, server);
+
+httpsServer.listen(ENV.ServerInnerPort, () => {
+  logger.info(SERVER_START_MSG);
+});
+
+httpsServer.on("error", err => {
+  logger.err(err.message);
 });
